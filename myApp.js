d39dd6e9e62b85df342888a390edd4d602535cff
Hawijo <joshuahawi24@gmail.com>
2025-04-09 13:19:10 +0300

    Adding person schema

--- myApp.js
+++ myApp.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-let Person;
+let Person = personSchema;
+
+const personSchema = new mongoose.Schema ({
+  name: { type: String, required: true },
+  age: [Number],
+  favoriteFoods: [String]
+})
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
