296aff386b533d7b6ab7405fe5b1a67711cfdebd
Hawijo <joshuahawi24@gmail.com>
2025-04-10 09:56:38 +0300

    personSchema bug fixes

--- myApp.js
+++ myApp.js
@@ -2,14 +2,15 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const Schema = mongoose.Schema
 
-const personSchema = new mongoose.Schema ({
+const personSchema = new Schema ({
   name: { type: String, required: true },
-  age: [Number],
+  age: Number,
   favoriteFoods: [String]
 })
 
-let Person = personSchema;
+let Person = mongoose.model("Person", personSchema);
 
 
 const createAndSavePerson = (done) => {
