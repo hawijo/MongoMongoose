0f9ce47614fbd6d3c784b0c7c47151959ee78f0d
Hawijo <joshuahawi24@gmail.com>
2025-04-10 13:35:29 +0300

    Find people by id pt 2

--- myApp.js
+++ myApp.js
@@ -64,7 +64,7 @@ const findOneByFood = (food, done) => {
 const findPersonById = (personId, done) => {
   Person.findById({ _id: personId }, function (err, data) {
     if (err) return console.error(err);
-    done(null /*, data*/);
+    done(null, data);
   });
 };
 
