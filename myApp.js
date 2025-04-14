d453b434c046fa49b57a0baa6cda96f003d357a4
Hawijo <joshuahawi24@gmail.com>
2025-04-14 13:03:43 +0300

    Adding correctcode

--- myApp.js
+++ myApp.js
@@ -70,15 +70,21 @@ const findPersonById = (personId, done) => {
 
 /** 8) Find, Edit Then Save Person */
 const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
-  Person.findById({ _id: personId}, function (err, data) {
-    if (err) return console.error(err);
-    Person.favoriteFoods.push(foodToAdd);
-    Person.save(function (err, data) {
-      if (err) return console.error(err);
-      done(null, data);
-    });
-  });
+  const foodToAdd = 'hamburger';
+
+  // .findById() method to find a person by _id with the parameter personId as search key. 
+  Person.findById(personId, (err, person) => {
+    if (err) return console.log(err);
+
+    // Array.push() method to add "hamburger" to the list of the person's favoriteFoods
+    person.favoriteFoods.push(foodToAdd);
+
+    // and inside the find callback - save() the updated Person.
+    person.save((err, updatedPerson) => {
+      if (err) return console.log(err);
+      done(null, updatedPerson)
+    })
+  })
 };
 
 const findAndUpdate = (personName, done) => {
